1b7d751ac4dae4ef06cfe98819e58e321f459184
iaeliatravel <iaeliatravel@gmail.com>
2026-05-25 13:28:55 +0100

    Update HotelForm.tsx

--- src/components/services/HotelForm.tsx
+++ src/components/services/HotelForm.tsx
@@ -1,7 +1,13 @@
 'use client'
 import { useEffect } from 'react'
 import { QuoteItem } from '@/types/database'
+
+// src/components/services/HotelForm.tsx
 import { buildHotelDescription } from '@/lib/description-builder'
+
+// src/components/services/FlightForm.tsx
+import { buildFlightDescription } from '@/lib/description-builder'
+
 import AutocompleteInput from '@/components/AutocompleteInput'
 import NumberStepper from '@/components/NumberStepper'
 import SelectField from '@/components/SelectField'
